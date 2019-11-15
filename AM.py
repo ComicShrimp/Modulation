90b72c3232255e82b46ed394f67190ae8493ab2d
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2019-11-15 00:17:21 -0300

    Modulação AM consertada

--- AM.py
+++ AM.py
@@ -2,32 +2,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Funções Importantes
-def sinalBandaBase():
-    return 1
-
-
-def sinalPortadora():
-    return 2
-
-
 # Função Main a ser executada
-
-# Apenas Um Aviso para caso as importações deem errado
 
 # Variaveis Globais - Estas Variaveis alteram todos os valores
 
-tempo_maximo = 4500
-frequencia_moduladora = 1
-frequencia_portadora = 10
+tempo_maximo = 45000
 indice_modulacao = 1
+
+frequencia_mensagem = 5
+amplitude_mensagem = 1
+
+frequencia_portadora = 40
+amplitude_portadora = 1
 
 # Vai dividir cada valor do vetor criado para que se obtenha valores pequenos
 tempo = np.arange(tempo_maximo) / tempo_maximo
-mensagem = np.sin(2 * np.pi * frequencia_moduladora * tempo) * indice_modulacao
+
+# Cria a onda da portadora e da mensagem
+mensagem = np.sin(2 * np.pi * frequencia_mensagem * tempo)
 portadora = np.cos(2 * np.pi * frequencia_portadora * tempo)
-modulado = np.multiply(mensagem, portadora)
-demodulado = np.divide(modulado, portadora)
+
+# Implementa a equção: s(t) = Ac * [1 + Ka * m(t)] * cos(2 * pi * fc * t)
+modulado = np.multiply(np.multiply(amplitude_portadora, np.add(
+    indice_modulacao, np.multiply(amplitude_mensagem, mensagem))), portadora)
 
 # Gerar Graficos
 
@@ -51,14 +48,6 @@ plt.grid(True)
 plt.subplot(2, 2, 3)
 plt.title('Sinal Modulado')
 plt.plot(modulado)
-plt.xlabel('Amplitude')
-plt.ylabel('Tempo')
-plt.grid(True)
-
-# Sinal Demodulado
-plt.subplot(2, 2, 4)
-plt.title('Sinal Demodulado')
-plt.plot(demodulado)
 plt.xlabel('Amplitude')
 plt.ylabel('Tempo')
 plt.grid(True)
